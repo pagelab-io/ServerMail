068b449718cc9129092a1d3274e797482a4abe4c
danielpro4 <daniel.perez.atanacio@gmail.com>
2015-12-10 14:51:00 -0600

    style: .form-control.done rule css added

--- resources/views/api/tasks/app.blade.php
+++ resources/views/api/tasks/app.blade.php
@@ -43,7 +43,7 @@
                             <td>
                                 <input type="text"
                                        class="form-control"
-                                       ng-class="{done: task.done == 0}"
+                                       ng-class="{done: task.done != 0}"
                                        ng-model="task.name"
                                        ng-blur="controller.updateTask(task)"
                                        enter-stroke="controller.updateTask(task)" />
